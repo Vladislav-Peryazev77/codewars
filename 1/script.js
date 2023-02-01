b0f1bbe5ea7c44961bda5c9bcbb21f24da49fba4
vladislav77 <vladperyazev14@gmail.com>
2023-02-01 23:20:14 +0300

    Digit*Digit ( 7 kuy)

--- 1/script.js
+++ 1/script.js
@@ -4,10 +4,11 @@
 
 // Example #2: An input of 765 will/should return 493625 because 72 is 49, 
 // 62 is 36, and 52 is 25. (49-36-35)
-function func(num) {
+function squareDigits(num){
 	let arr = String(num).split('');
-	let result = arr.map(item => item * item);
-	return num = result.join('');
-}
-console.log(func(3212));
+	  let result = arr.map(item => item * item);
+	  return num = +result.join('');
+  }
+
+// console.log(func(3212));
 
