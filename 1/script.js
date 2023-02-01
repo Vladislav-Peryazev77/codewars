2357d0af9181bf4de63e896faaf116082d974c24
vladislav77 <vladperyazev14@gmail.com>
2023-02-01 23:17:58 +0300

    7 kyu Digit*Digit

---
+++ 1/script.js
@@ -0,0 +1,13 @@
+'use strict';
+// For example, if we run 9119 through the function, 811181 will come out, 
+// because 92 is 81 and 12 is 1. (81-1-1-81)
+
+// Example #2: An input of 765 will/should return 493625 because 72 is 49, 
+// 62 is 36, and 52 is 25. (49-36-35)
+function func(num) {
+	let arr = String(num).split('');
+	let result = arr.map(item => item * item);
+	return num = result.join('');
+}
+console.log(func(3212));
+
