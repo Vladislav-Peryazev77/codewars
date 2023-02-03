fa668fd6e846bea185bbaef9960d1c60d3ca2fb7
vladislav77 <vladperyazev14@gmail.com>
2023-02-03 19:03:58 +0300

    Counting Duplicates (6kuy)

---
+++ 3/script.js
@@ -0,0 +1,37 @@
+'use strict';
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+function duplicateCount(text){
+  
+  let obj = {};
+  let count = 0;
+  
+  for ( let i of text) {
+    i = i.toLowerCase();
+    if (!obj[i]) {
+      obj[i] = 1;
+    } else {
+      obj[i]++;
+    }
+  }
+  
+  for ( let i in obj) {
+    if ( obj[i] > 1) {
+      count++;
+    }
+  }
+  return count;
+}
+
+
+
+
