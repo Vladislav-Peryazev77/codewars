2a61dab0141499c513c106b7229ead5eb95cdbe6
vladislav77 <vladperyazev14@gmail.com>
2023-03-03 12:32:08 +0300

    Convert boolean values to string Yes or No(8kyu)

---
+++ 19/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord( bool ){
+  return bool ? 'Yes' : 'No'; 
+}
+
+
+
+
+
+
+
+
+
