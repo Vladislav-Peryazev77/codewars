20aa0ec0cb222699ddef2993d5c8eb156d7d8130
vladislav77 <vladperyazev14@gmail.com>
2023-03-05 21:41:58 +0300

    Which color is the brightest? (7kyu)

---
+++ 25/script.js
@@ -0,0 +1,40 @@
+"use strict";
+// One of the common ways of representing color is the RGB color model, in which the Red, Green, and Blue primary colors of light are added together in various ways to reproduce a broad array of colors.
+
+// One of the ways to determine brightness of a color is to find the value V of the alternative HSV (Hue, Saturation, Value) color model. Value is defined as the largest component of a color:
+
+// V = max(R,G,B)
+// You are given a list of colors in 6-digit hexidecimal notation #RRGGBB. Return the brightest of these colors!
+
+// For example,
+
+// brightest(["#001000", "#000000"]) == "#001000"
+// brightest(["#ABCDEF", "#123456"]) == "#ABCDEF"
+// If there are multiple brightest colors, return the first one:
+
+// brightest(["#00FF00", "#FFFF00", "#01130F"]) == "#00FF00"
+// Note that both input and output should use upper case for characters A, B, C, D, E, F.
+function brightest(colors){
+  let result = '';
+  let maxValue = 0;
+  colors.forEach((color, index) => {
+    let r = parseInt(color.slice(1,3), 16);
+    let g = parseInt(color.slice(3,5), 16);
+    let b = parseInt(color.slice(5,7), 16);
+    let value = Math.max(r,g,b);
+    if ( value > maxValue) {
+      maxValue = value;
+      result = colors[index];
+    }
+  })
+  return result;
+}
+console.log(brightest(["#001000", "#000000"]));
+
+
+
+
+
+
+
+
