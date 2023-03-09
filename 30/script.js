e6df519df9ad3414dfc50090eef22fd6294048e9
vladislav77 <vladperyazev14@gmail.com>
2023-03-09 21:10:52 +0300

    Head, tail, init and Last(7kyu)

---
+++ 30/script.js
@@ -0,0 +1,45 @@
+"use strict";
+// $ ghci
+// GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+// λ head [1,2,3,4,5]
+// 1
+// λ tail [1,2,3,4,5]
+// [2,3,4,5]
+// λ init [1,2,3,4,5]
+// [1,2,3,4]
+// λ last [1,2,3,4,5]
+// 5
+// Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+
+// | HEAD | <----------- TAIL ------------> |
+// [  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+// | <----------- INIT ------------> | LAST |
+
+// head [x] = x
+// tail [x] = []
+// init [x] = []
+// last [x] = x
+// Here's how I expect the functions to be called in your language:
+
+// head([1,2,3,4,5]); => 1
+// tail([1,2,3,4,5]); => [2,3,4,5]
+function head(arr) {
+  return arr[0];
+}
+function tail(arr) {
+  return arr.slice(1, arr.length);
+}
+function init(arr) {
+  return arr.slice(0, arr.length - 1);
+}
+function last(arr) {
+  return arr[arr.length - 1];
+}
+
+
+
+
+
+
+
+
